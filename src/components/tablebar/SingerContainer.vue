ad3c6c5fa4e4cce9376cfc0168fda82668ad0874
“”冯春炜“ <1197764821@qq.com>
2020-05-08 22:38:54 +0800

    渲染歌手列表

--- src/components/tablebar/SingerContainer.vue
+++ src/components/tablebar/SingerContainer.vue
@@ -1,58 +1,45 @@
 <template>
   <div>
     <ul class="mui-table-view">
-      <li class="mui-table-view-cell mui-media" v-for="item in songerList" :key="item.id">
+      <li class="mui-table-view-cell mui-media" v-for="item in singerList" :key="item.id">
         <router-link href="javascript:;" to="Musicers">
-          <img class="mui-media-object mui-pull-left" :src="infoObj.avatar_s500" />
+          <img class="mui-media-object mui-pull-left" :src="item.avatar_s500" />
           <div class="mui-media-body">
-            <p class="mui-ellipsis">{{infoObj.name}}</p>
+            <p class="mui-ellipsis">{{item.name}}</p>
           </div>
         </router-link>
       </li>
     </ul>
-    <!-- <div class="box" v-for="item in songerList" :key="item.id">
-      <img  :src="item.avatar_s500" alt="">
-      <div class="inner">{{infoObj.name}}11111111</div>
-    </div>-->
+
   </div>
 </template> 
 <script>
 export default {
   data() {
     return {
-      songerList: [],
-      songerList1: [],
-      infoObj: {
-        name: "",
-        avatar_s500: ""
-      }
+      singeridList: ["2517"  ,"7994","1091","45561","2507","245815","1077","1204","1117","82366"],
+      singerList:[]
     };
   },
   created() {
     this.getmusicerList();
-    // this.getsongerList1();
   },
   methods: {
-    // getsongerList1() {
-    //   this.songerList.forEach(item => {
-    //     this.songerList1.push(item);
-    //   });
-    //   console.log(this.songerList1);
-    // },
     getmusicerList() {
-      const musicerList =
+      for(let item of this.singeridList){
+        const musicerList =
         this.HOST +
-        "v1/restserver/ting?method=baidu.ting.artist.getInfo&tinguid=2517";
+        "v1/restserver/ting?method=baidu.ting.artist.getInfo&tinguid="+item;
       this.$axios
         .get(musicerList)
         .then(result => {
-          this.infoObj.name = result.data.name;
-          this.infoObj.avatar_s500 = result.data.avatar_s500;
-          console.log(this.infoObj.name);
-          this.songerList = result.data;
-          console.log(result);
+          const infoObj = {}
+          infoObj.name = result.data.name;
+          infoObj.avatar_s500 = result.data.avatar_s500;
+          this.singerList.push(infoObj)
         })
         .catch();
+      }
     }
   }
 };
@@ -63,7 +50,5 @@ li {
 }
 .mui-table-view img {
   border-radius: 50%;
-
 }
-
 </style>
